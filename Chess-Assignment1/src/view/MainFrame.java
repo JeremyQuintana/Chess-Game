8403e80831cab1fbf90dd9d998341ca78dd87b45
jeremykane <jeremykane@10.132.112.246>
2019-05-01 12:01:17 +1000

    Update MainFrame

--- Chess-Assignment1/src/view/MainFrame.java
+++ Chess-Assignment1/src/view/MainFrame.java
@@ -1,5 +1,13 @@
 package view;
 
-public class MainFrame {
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame{
+	
+	public MainFrame() {
+		super("Chess Game");
+		setBounds(100, 100, 640, 480);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
 }
