8a8bfa13cafdeb9cf34ac2d1312dcd0ee9f002f0
jeremykane <jeremykane@192-168-1-198.tpgi.com.au>
2019-05-08 00:21:36 +1000

    Updated Piece GUI for each Tiles and Updating new Images Icon

--- Chess-Assignment1/src/view/BoardPanel.java
+++ Chess-Assignment1/src/view/BoardPanel.java
@@ -3,47 +3,92 @@ package view;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel{
-	final List<TilePanel> tilesList;
+	final List<CellPanel> cellList;
 		
 	public BoardPanel() {
 		super(new GridLayout(6,6));
-		tilesList = new ArrayList<>();
+		cellList = new ArrayList<>();
 		for(int i = 0; i < 36; i++) {
-			TilePanel tilePanel = new TilePanel(this, i);
-			tilesList.add(tilePanel);
+			CellPanel tilePanel = new CellPanel(this, i);
+			cellList.add(tilePanel);
 			add(tilePanel);
 		}
 		setPreferredSize(new Dimension(400,350));
+		putAllPieces();
 		validate();
 	}
 	
-	private class TilePanel extends JPanel{
+	private void drawThePiece(int i, String pieceType, String worb) {
+		ImageIcon piece = null; 
+		JLabel pieceLabel = new JLabel();
 		
-		private final int tileId;
-		public TilePanel(final BoardPanel boardPanel, final int tileId) {
+		pieceLabel.setSize(100,100);
+		
+		CellPanel draw = cellList.get(i);
+		piece = new ImageIcon("images/"+pieceType+"_"+worb+".png");
+		Image convertImageIcon = piece.getImage();
+		Image resizeImage = convertImageIcon.getScaledInstance(80, 70, Image.SCALE_SMOOTH);
+		ImageIcon resized = new ImageIcon(resizeImage);
+			
+		pieceLabel.setIcon(resized);
+		pieceLabel.setHorizontalAlignment(JLabel.CENTER);
+		draw.add(pieceLabel);
+	}
+	
+	private void putAllPieces() {
+		for(int i = 0; i < 6 ; i++) {
+			drawThePiece(i,intToPiece(i),"w");
+		}
+		
+		for(int j = 30; j < 36 ; j++) {
+			drawThePiece(j,intToPiece(j),"b");
+		}
+		
+	}
+	
+	private String intToPiece(int i) {
+		String result = null;
+		
+		if(i == 0 || i == 5 || i == 30 || i == 35) {result = "rook";}
+		else if(i == 1 || i == 4 || i == 31 || i == 34) {result = "bishop";}
+		else {result = "knight";}
+		
+		return result;
+	}
+	
+	private class CellPanel extends JPanel{
+		
+		private final int cellId;
+		public CellPanel(final BoardPanel boardPanel, final int cellId) {
 			super(new GridLayout());
-			this.tileId = tileId;
+			this.cellId = cellId;
 			setPreferredSize(new Dimension(10,10));
 			colortheTile();
 			validate();
 		}
 		
 		private void colortheTile() {
-			if(this.tileId>= 0 && this.tileId <6 || this.tileId >= 12 && this.tileId <18 || this.tileId >= 24 && this.tileId <30){
-				setBackground(this.tileId % 2 == 0 ? new Color(255, 230, 204) : new Color(77, 40, 0));
+			if(this.cellId>= 0 && this.cellId <6 || this.cellId >= 12 && this.cellId <18 || this.cellId >= 24 && this.cellId <30){
+				setBackground(this.cellId % 2 == 0 ? new Color(255, 230, 204) : new Color(77, 40, 0));
 			}
 			else {
-				setBackground(this.tileId % 2 != 0 ? new Color(255, 230, 204) : new Color(77, 40, 0));
+				setBackground(this.cellId % 2 != 0 ? new Color(255, 230, 204) : new Color(77, 40, 0));
 			}
 			
 		}
-
 		
 	}
 
--- Chess-Assignment1/src/view/SidePanel.java
+++ Chess-Assignment1/src/view/SidePanel.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
