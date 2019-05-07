392469dc7e24b6d49a58244e2a0635cb342048f8
Reuben <Reuben@DESKTOP-OF5LO3E>
2019-05-07 22:34:07 +1000

    ready to add multiPiece

--- Chess-Assignment1/src/model/Piece.java
+++ Chess-Assignment1/src/model/Piece.java
@@ -14,13 +14,15 @@ public abstract class Piece {
 		this.key = key;
 	}
 
+	
 	private PieceType pieceType;
 	private Player player;
 	private String key;
-	//how to get the cell and WHY to get the cell
 	private Cell location;
 	// all pieces have a maximum of 2 moves they can make
 	private static final int MOVE_LIMIT = 3;
+	
+	
 	
 	PieceType getPieceType()
 	{
@@ -39,30 +41,6 @@ public abstract class Piece {
 	{
 		return location;
 	}
-	
-	boolean isValidMove(Cell destination)
-	{
-		moveUpdate(destination);
-		if (destination == null)
-			return false;
-		if (destination.getIsOccupied())
-			return player.getType() != destination.getOccupiedType();
-		return true;
-	}
-	
-	// adds destination if it fits rules, and breaks out of loop if no more to be added
-	boolean movesLeftToAdd(int a)
-	{
-		if (a > Piece.MOVE_LIMIT)
-			return false;
-		return movesLeftToAdd2(a);
-		
-	}
-	
-	abstract void moveUpdate(Cell destination);
-	abstract boolean movesLeftToAdd2(int a);
-	
-	
 	
 	void move(Cell destination)
 	{
@@ -83,9 +61,6 @@ public abstract class Piece {
 		return key;
 	}
 	
-	
-	
-	
 	// get row/column depending on its move pattern and current iteration a
 	abstract int getDestinationRow(int a, boolean rowPositive, boolean colPositive);
 	abstract int getDestinationCol(int a, boolean rowPositive, boolean colPositive);
@@ -104,6 +79,23 @@ public abstract class Piece {
 		return newCol;
 	}
 	
+	boolean isValidMove(Cell destination)
+	{
+		if (destination == null)
+			return false;
+		if (destination.getIsOccupied())
+			return player.getType() != destination.getOccupiedType();
+		return true;
+	}
+	
+	// adds destination if it fits rules, and breaks out of loop if no more to be added
+	boolean movesLeftToAdd(int a, Cell destination)
+	{
+		if (a > Piece.MOVE_LIMIT)
+			return false;
+		return movesLeftToAdd2(a, destination);
+	}
+	abstract boolean movesLeftToAdd2(int a, Cell destination);
 	
 }
 
